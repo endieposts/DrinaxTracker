ffe297198abc9816f126312ceb84da87ba99e349
endieposts <13526487+endieposts@users.noreply.github.com>
2025-02-23 17:04:46 +0000

    db connecting

---
+++ src/main/java/com/endie/drinaxtracker/model/User.java
@@ -0,0 +1,18 @@
+package com.endie.drinaxtracker.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+    // Getters and Setters
+}
