6e46e58da33053dd7bb6fdf52cff695f87a34d83
endieposts <13526487+endieposts@users.noreply.github.com>
2025-02-23 17:41:49 +0000

    use h2 file data source so I can roam

--- src/main/java/com/endie/drinaxtracker/model/User.java
+++ src/main/java/com/endie/drinaxtracker/model/User.java
@@ -3,7 +3,7 @@ package com.endie.drinaxtracker.model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
 
     @Id
--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO user (username, password, email, role) VALUES ('admin', 'testpassword', 'test@test.com', 1);
-INSERT INTO user (username, password, email, role) VALUES ('gm1', 'testpassword', 'test2@test.com', 2);
-INSERT INTO user (username, password, email, role) VALUES ('gm2', 'testpassword', 'test3@test.com', 2);
-INSERT INTO user (username, password, email, role) VALUES ('player1', 'testpassword', 'test4@test.com', 3);
+INSERT INTO users (username, password, email, role) VALUES ('admin', 'testpassword', 'test@test.com', 1);
+INSERT INTO users (username, password, email, role) VALUES ('gm1', 'testpassword', 'test2@test.com', 2);
+INSERT INTO users (username, password, email, role) VALUES ('gm2', 'testpassword', 'test3@test.com', 2);
+INSERT INTO users (username, password, email, role) VALUES ('player1', 'testpassword', 'test4@test.com', 3);
