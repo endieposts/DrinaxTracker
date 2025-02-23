f883bd9e3ca1f3ec7d22913570772bd1aeddeca9
endieposts <13526487+endieposts@users.noreply.github.com>
2025-02-23 10:52:04 +0000

    Fix Spring config issue

---
+++ src/main/java/com/endie/drinaxtracker/DrinaxTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.endie.drinaxtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinaxTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinaxTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/endie/drinaxtracker/DrinaxTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endie.drinaxtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinaxTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
