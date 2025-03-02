6411a6a0573508ce160c2024a59006ea27c40e39
endieposts <13526487+endieposts@users.noreply.github.com>
2025-03-02 11:24:26 +0000

    Got validation working. Added endpoints for user.

---
+++ src/main/java/com/endie/drinaxtracker/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.endie.drinaxtracker.controller;
+
+import com.endie.drinaxtracker.model.User;
+import com.endie.drinaxtracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
+    }
+
+    @PostMapping("/users")
+    public User createUser(@Valid @RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@PathVariable Long id, @Valid @RequestBody User userRequest) {
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(userRequest.getUsername());
+            user.setPassword(userRequest.getPassword());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        return userRepository.findById(id).map(user -> {
+            userRepository.delete(user);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
+    }
+
+
+}
--- src/main/java/com/endie/drinaxtracker/model/User.java
+++ src/main/java/com/endie/drinaxtracker/model/User.java
@@ -82,5 +82,5 @@ public class User {
                 + role + "]";
     }
 
-    
+
 }
