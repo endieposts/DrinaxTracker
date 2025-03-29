a7ff3fac430212d115929cf089bf3772847f2dd4
endieposts <13526487+endieposts@users.noreply.github.com>
2025-03-29 12:58:02 +0000

    Star star system model/repo

---
+++ src/main/java/com/endie/drinaxtracker/repository/StarSystemRepository.java
@@ -0,0 +1,8 @@
+package com.endie.drinaxtracker.repository;
+
+import com.endie.drinaxtracker.model.StarSystem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StarSystemRepository  extends JpaRepository<StarSystem, Long> {
+
+}
